97c49ee6f4447360d47e7a985a480f8216151ec5
root <root@SYNNTIDEV20.SynetekSolutions.local>
2022-10-12 16:53:26 -0400

    Bundle updgrade from symfony 4.4 to 5.4

--- Command/CheckCommand.php
+++ Command/CheckCommand.php
@@ -2,13 +2,24 @@
 
 namespace NTI\EmailBundle\Command;
 
-use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use NTI\EmailBundle\Service\Mailer;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
-class CheckCommand extends ContainerAwareCommand
+class CheckCommand extends Command
 {
+    private $container;
+
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+
+        parent::__construct();
+    }
+
+
     protected function configure()
     {
         $this
@@ -19,7 +30,12 @@ class CheckCommand extends ContainerAwareCommand
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $this->getContainer()->get('nti.mailer')->check($output);
+       try{
+            $this->container->get('nti.mailer')->check($output);
+            return 0;
+        } catch(\Exception $e){
+            return 1;
+        }
     }
 
 }
